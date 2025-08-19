273b698b220d8abef910308939f6f2880b339f22
SharmaDevanshu089 <SharmaDevanshu089@gmail.com>
2025-08-19 23:02:55 +0530

    Question Commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    println!("Enter a number:");
+
+    io::stdin().read_line(&mut input).unwrap();
+
+    // Try to parse input into i32
+    let num = input.trim().parse::<i32>();  
+
+    // ❓ Your task: handle Result here with match
+    // If Ok(n) → print "You entered n"
+    // If Err(e) → print "Invalid input!"
 }
